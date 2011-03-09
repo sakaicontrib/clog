33e759920c795c04e7e770fad6388ea5eea79988
a.fish@lancaster.ac.uk <a.fish@lancaster.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2011-03-09 23:12:28 +0000

    CLOG-32
    Removed unused translations

--- tool/src/webapp/clog-translations-es.js
+++ tool/src/webapp/clog-translations-es.js
@@ -1,66 +1,18 @@
-lastEntries="�ltimas entradas";
 title="Blogger";
 search="B�squeda";
 edit="Editar";
 save="Guardar";
 show="Mostrar";
-preview="Previsualizar";
-showAll="Mostrar todos";
 clog_home_label="Inicio";
 edit="Editar";
 clog_create_post_label="Nuevo";
 search="Buscar";
 del="Borrar";
 cancel="Cancelar";
-msgs.applyOnNonPrivate=" Aplicable en entradas no privadas";
-clog_read_only_label="S�lo lectura";
-note="Nota";
-noteImages="Solamente los ficheros JPEG y GIF menores de 4 MB se mostrar�n como im�genes. Otros formatos ser�n tratados como ficheros adjuntos.";
-maxFileSize="El tama�o m�ximo permitido es de 20 MB.";
-
-currentStructure="Estructura actual";
 clog_allow_comments_label="Permitir comentarios";
-addComment="A�adir comentarios";
-yourComment="Su comentario";
-last20="�ltimas 20 contribuciones";
-pagerPrev="Anterior";
-pagerNext="Siguiente";
-filterByVisibility="Filtrar por el tipo de acceso";
-applyFilter="Aplicar filtro";
 clog_delete_post_message="�Est� seguro de que desea borrar la entrada?";
-
-exampleURL="Ejemplo http://www.mypage.co.uk";
-
-
 clog_post_editor_label="Editor de entrada";
-
 clog_title_label="T�tulo";
-keywords="Palabras clave";
-abstract="Resumen";
-postDate="Fecha";
-postMainText="Cuerpo";
-postCreator="Creador";
-clog_visibility_label="Acceso";
-postComments="Comentarios";
-
 text="Texto";
-images="Im�genes";
-links="Enlaces";
-files="Ficheros";
-
-index="�ndice";
-element="Elemento";
 commands="Comandos";
-linkDescription="Descripci�n del enlace";
-linkURL="Enlace a la p�gina web (URL)";
-
-imageName="Nombre de la imagen";
 image="Imagen";
-description="Descripci�+addToDocument=A�adir al documento";
-modifyInDocument="Modificar en el documento (�ndice";
-fileName="Nombre del fichero";
-resetEditor="Limpiar editor";
-
-state_all="Todos";
-state_private="Privados";
-state_site="S�lo los del sitio";
--- tool/src/webapp/clog-translations-fr-ca.js
+++ tool/src/webapp/clog-translations-fr-ca.js
@@ -1,107 +1,16 @@
-
-abstract =" R\u00E9sum\u00E9";
-
-addComment =" Ajouter commentaire";
-
-addToDocument =" Ajouter au document";
-
 clog_allow_comments_label =" Permettre commentaires";
-
-applyFilter =" Appliquer filtre";
-
 cancel =" Annuler";
-
 commands =" Commandes";
-
-currentStructure =" Structure actuelle";
-
 del =" Supprimer";
-
-description =" Description";
-
 edit =" \u00C9diter";
-
-element =" \u00C9l\u00E9ment";
-
-exampleURL =" Exemple http://www.mypage.co.uk";
-
-fileName =" Nom du fichier";
-
-files =" Fichiers";
-
-filterByVisibility =" Filtrer par type d'acc\u00E8s";
-
 clog_home_label =" Accueil";
-
 image =" Image";
-
-imageName =" Nom de l'image";
-
-images =" Images";
-
-index =" Index";
-
-keywords =" Mots-cl\u00E9s";
-
-last20 =" Les derniers 20 envois";
-
-lastEntries =" Derni\u00E8res entr\u00E9es";
-
-linkDescription =" Description du lien";
-
-linkURL =" Hyperlien";
-
-links =" Liens";
-
-maxFileSize =" Taille maximale permise de 20 MO.";
-
-modifyInDocument =" Modifier dans le document (index";
-
-msgs.applyOnNonPrivate =" Applicable dans des entr\u00E9es non priv\u00E9es";
-
 clog_create_post_label =" Nouveau";
-
-note =" Note";
-
-noteImages =" Seulement les JPEG et les GIF plus petits que 4 MO seront montr\u00E9s en tant qu'images. Les autres formats seront trait\u00E9s comme des fichiers joints.";
-
-pagerNext =" Suiv";
-
-pagerPrev =" Pr\u00E9c.";
-
-postComments =" Commentaires";
-
-postCreator =" Cr\u00E9ateur";
-
-postDate =" Date";
-
 clog_post_editor_label =" \u00C9diteur d'envois";
-
-postMainText =" Corps";
-
 clog_title_label =" Titre";
-
-clog_visibility_label =" Acc\u00E8s";
-
-preview =" Aper\u00E7u";
-
-clog_read_only_label =" Lecture seule";
-
-resetEditor =" R\u00E9initialiser l'\u00E9diteur";
-
 save =" Enregistrer";
-
 search =" Recherche";
-
 show =" Montrer";
-
-showAll =" Montrer Tous";
-
 clog_delete_post_message =" \u00CAtes-vous certain de vouloir supprimer cet envoi?";
-
 text =" Texte";
-
 title =" Blogger";
-
-yourComment =" Votre commentaire";
-invalid_post_title="You must add a title. The title cannot contain xml markup.";
