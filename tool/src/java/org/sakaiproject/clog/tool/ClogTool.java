92596e33c5c62ab2e4dbb72622139dca8b6d3e64
a.fish@lancaster.ac.uk <a.fish@lancaster.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2011-06-30 08:49:14 +0000

    Removed clogManager as it was unused. Source format.

--- tool/src/java/org/sakaiproject/clog/tool/ClogTool.java
+++ tool/src/java/org/sakaiproject/clog/tool/ClogTool.java
@@ -19,7 +19,6 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 import org.apache.log4j.Logger;
-import org.sakaiproject.clog.api.ClogManager;
 import org.sakaiproject.clog.api.SakaiProxy;
 import org.sakaiproject.component.api.ComponentManager;
 import org.sakaiproject.search.api.InvalidSearchQueryException;
@@ -30,176 +29,174 @@ import org.sakaiproject.util.ResourceLoader;
  * @author Adrian Fish (a.fish@lancaster.ac.uk)
  */
 public class ClogTool extends HttpServlet {
-    private Logger logger = Logger.getLogger(getClass());
+	private Logger logger = Logger.getLogger(getClass());
 
-    private SakaiProxy sakaiProxy;
-    private ClogManager clogManager = null;
+	private SakaiProxy sakaiProxy;
 
-    public void init(ServletConfig config) throws ServletException {
-	super.init(config);
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
 
-	if (logger.isDebugEnabled())
-	    logger.debug("init");
+		if (logger.isDebugEnabled())
+			logger.debug("init");
 
-	ComponentManager componentManager = org.sakaiproject.component.cover.ComponentManager.getInstance();
-	sakaiProxy = (SakaiProxy) componentManager.get(SakaiProxy.class);
-	clogManager = (ClogManager) componentManager.get(ClogManager.class);
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	if (logger.isDebugEnabled())
-	    logger.debug("doGet()");
-
-	if (sakaiProxy == null)
-	    throw new ServletException("sakaiProxy MUST be initialised.");
-
-	String state = request.getParameter("state");
-	String postId = request.getParameter("postId");
-
-	if (state == null)
-	    state = "viewAllPosts";
-
-	if (postId == null)
-	    postId = "none";
-
-	String siteId = sakaiProxy.getCurrentSiteId();
-
-	String userId = sakaiProxy.getCurrentUserId();
-
-	if (userId == null) {
-	    // We are not logged in. Could be the gateway placement.
-	    if (!"!gateway".equals(siteId)) {
-		// There should be an authenticated user at this point.
-		throw new ServletException("getCurrentUser returned null.");
-	    }
+		ComponentManager componentManager = org.sakaiproject.component.cover.ComponentManager.getInstance();
+		sakaiProxy = (SakaiProxy) componentManager.get(SakaiProxy.class);
 	}
 
-	String toolId = sakaiProxy.getCurrentToolId();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if (logger.isDebugEnabled())
+			logger.debug("doGet()");
 
-	// We need to pass the language code to the JQuery code in the pages.
-	Locale locale = (new ResourceLoader(userId)).getLocale();
-	String languageCode = locale.getLanguage();
-	
-	// CLOG-44
-	if("".equals(languageCode)) {
-	    languageCode = "en";
-	}
+		if (sakaiProxy == null)
+			throw new ServletException("sakaiProxy MUST be initialised.");
 
-	String pathInfo = request.getPathInfo();
+		String state = request.getParameter("state");
+		String postId = request.getParameter("postId");
 
-	boolean publicAllowed = sakaiProxy.isPublicAllowed();
+		if (state == null)
+			state = "viewAllPosts";
 
-	if (pathInfo == null || pathInfo.length() < 1) {
-	    String uri = request.getRequestURI();
+		if (postId == null)
+			postId = "none";
 
-	    // There's no path info, so this is the initial state
-	    if (uri.contains("/portal/pda/")) {
-		// The PDA portal is frameless for redirects don't work. It also
-		// means that we can't pass url parameters to the page.We can
-		// use a cookie and the JS will pull the initial state from that
-		// instead.
-		Cookie params = new Cookie("sakai-tool-params", "state=" + URLEncoder.encode(state,"UTF-8") + "&siteId=" + siteId + "&placementId=" + toolId + "&postId=" + URLEncoder.encode(postId,"UTF-8") + "&langage=" + languageCode + "&publicAllowed=" + publicAllowed);
-		response.addCookie(params);
+		String siteId = sakaiProxy.getCurrentSiteId();
 
-		RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/clog.html");
-		dispatcher.include(request, response);
-		return;
-	    } else {
-		String url = "/clog-tool/clog.html?state=" + URLEncoder.encode(state,"UTF-8") + "&siteId=" + siteId + "&placementId=" + toolId + "&postId=" + URLEncoder.encode(postId,"UTF-8") + "&language=" + languageCode + "&publicAllowed=" + publicAllowed + "&editor=" + sakaiProxy.getWysiwygEditor();
-		response.sendRedirect(url);
-		return;
-	    }
-	} else {
-	    String[] parts = pathInfo.substring(1).split("/");
+		String userId = sakaiProxy.getCurrentUserId();
 
-	    if (parts.length >= 1) {
-		String part1 = parts[0];
-
-		if ("perms".equals(part1)) {
-		    doPermsGet(response);
+		if (userId == null) {
+			// We are not logged in. Could be the gateway placement.
+			if (!"!gateway".equals(siteId)) {
+				// There should be an authenticated user at this point.
+				throw new ServletException("getCurrentUser returned null.");
+			}
 		}
 
-		else if ("userPerms".equals(part1)) {
-		    doUserPermsGet(response);
+		String toolId = sakaiProxy.getCurrentToolId();
+
+		// We need to pass the language code to the JQuery code in the pages.
+		Locale locale = (new ResourceLoader(userId)).getLocale();
+		String languageCode = locale.getLanguage();
+
+		// CLOG-44
+		if ("".equals(languageCode)) {
+			languageCode = "en";
 		}
-	    }
+
+		String pathInfo = request.getPathInfo();
+
+		boolean publicAllowed = sakaiProxy.isPublicAllowed();
+
+		if (pathInfo == null || pathInfo.length() < 1) {
+			String uri = request.getRequestURI();
+
+			// There's no path info, so this is the initial state
+			if (uri.contains("/portal/pda/")) {
+				// The PDA portal is frameless for redirects don't work. It also
+				// means that we can't pass url parameters to the page.We can
+				// use a cookie and the JS will pull the initial state from that
+				// instead.
+				Cookie params = new Cookie("sakai-tool-params", "state=" + URLEncoder.encode(state, "UTF-8") + "&siteId=" + siteId + "&placementId=" + toolId + "&postId=" + URLEncoder.encode(postId, "UTF-8") + "&langage=" + languageCode + "&publicAllowed=" + publicAllowed);
+				response.addCookie(params);
+
+				RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/clog.html");
+				dispatcher.include(request, response);
+				return;
+			} else {
+				String url = "/clog-tool/clog.html?state=" + URLEncoder.encode(state, "UTF-8") + "&siteId=" + siteId + "&placementId=" + toolId + "&postId=" + URLEncoder.encode(postId, "UTF-8") + "&language=" + languageCode + "&publicAllowed=" + publicAllowed + "&editor=" + sakaiProxy.getWysiwygEditor();
+				response.sendRedirect(url);
+				return;
+			}
+		} else {
+			String[] parts = pathInfo.substring(1).split("/");
+
+			if (parts.length >= 1) {
+				String part1 = parts[0];
+
+				if ("perms".equals(part1)) {
+					doPermsGet(response);
+				}
+
+				else if ("userPerms".equals(part1)) {
+					doUserPermsGet(response);
+				}
+			}
+		}
 	}
-    }
 
-    private void doUserPermsGet(HttpServletResponse response) throws ServletException, IOException {
-	Set<String> perms = sakaiProxy.getPermissionsForCurrentUserAndSite();
-	JSONArray data = JSONArray.fromObject(perms);
-	response.setStatus(HttpServletResponse.SC_OK);
-	response.setContentType("application/json");
-	response.getWriter().write(data.toString());
-	response.getWriter().close();
-	return;
+	private void doUserPermsGet(HttpServletResponse response) throws ServletException, IOException {
+		Set<String> perms = sakaiProxy.getPermissionsForCurrentUserAndSite();
+		JSONArray data = JSONArray.fromObject(perms);
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.setContentType("application/json");
+		response.getWriter().write(data.toString());
+		response.getWriter().close();
+		return;
 
-    }
-
-    private void doPermsGet(HttpServletResponse response) throws ServletException, IOException {
-	Map<String, Set<String>> perms = sakaiProxy.getPermsForCurrentSite();
-	JSONObject data = JSONObject.fromObject(perms);
-	response.setStatus(HttpServletResponse.SC_OK);
-	response.setContentType("application/json");
-	response.getWriter().write(data.toString());
-	response.getWriter().close();
-	return;
-    }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	logger.info("doPost()");
-
-	String pathInfo = request.getPathInfo();
-
-	String[] parts = new String[] {};
-
-	if (pathInfo != null)
-	    parts = pathInfo.substring(1).split("/");
-
-	if (parts.length >= 1) {
-	    String part1 = parts[0];
-
-	    if ("search".equals(part1))
-		doSearchPost(request, response);
-	    else if ("setPerms".equals(part1))
-		doPermsPost(request, response);
 	}
-    }
 
-    private void doPermsPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	if (sakaiProxy.setPermsForCurrentSite(request.getParameterMap())) {
-	    response.setStatus(HttpServletResponse.SC_OK);
-	    response.setContentType("text/plain");
-	    response.getWriter().write("success");
-	    response.getWriter().close();
-	    return;
-	} else {
-	    response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-	    return;
+	private void doPermsGet(HttpServletResponse response) throws ServletException, IOException {
+		Map<String, Set<String>> perms = sakaiProxy.getPermsForCurrentSite();
+		JSONObject data = JSONObject.fromObject(perms);
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.setContentType("application/json");
+		response.getWriter().write(data.toString());
+		response.getWriter().close();
+		return;
 	}
-    }
 
-    private void doSearchPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	String searchTerms = request.getParameter("searchTerms");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		logger.info("doPost()");
 
-	if (searchTerms == null || searchTerms.length() == 0)
-	    throw new ServletException("No search terms supplied.");
+		String pathInfo = request.getPathInfo();
 
-	try {
-	    List<SearchResult> results = sakaiProxy.searchInCurrentSite(searchTerms);
-	
-	    JSONArray data = JSONArray.fromObject(results);
-	    response.setStatus(HttpServletResponse.SC_OK);
-	    response.setContentType("application/json");
-	    response.getWriter().write(data.toString());
-	    response.getWriter().close();
-	    return;
-	} catch(InvalidSearchQueryException isqe) {
-	    response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-	    response.getWriter().write("Your search terms were invalid");
-	    response.getWriter().close();
-	    return;
+		String[] parts = new String[] {};
+
+		if (pathInfo != null)
+			parts = pathInfo.substring(1).split("/");
+
+		if (parts.length >= 1) {
+			String part1 = parts[0];
+
+			if ("search".equals(part1))
+				doSearchPost(request, response);
+			else if ("setPerms".equals(part1))
+				doPermsPost(request, response);
+		}
 	}
-    }
+
+	private void doPermsPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if (sakaiProxy.setPermsForCurrentSite(request.getParameterMap())) {
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.setContentType("text/plain");
+			response.getWriter().write("success");
+			response.getWriter().close();
+			return;
+		} else {
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			return;
+		}
+	}
+
+	private void doSearchPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String searchTerms = request.getParameter("searchTerms");
+
+		if (searchTerms == null || searchTerms.length() == 0)
+			throw new ServletException("No search terms supplied.");
+
+		try {
+			List<SearchResult> results = sakaiProxy.searchInCurrentSite(searchTerms);
+
+			JSONArray data = JSONArray.fromObject(results);
+			response.setStatus(HttpServletResponse.SC_OK);
+			response.setContentType("application/json");
+			response.getWriter().write(data.toString());
+			response.getWriter().close();
+			return;
+		} catch (InvalidSearchQueryException isqe) {
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			response.getWriter().write("Your search terms were invalid");
+			response.getWriter().close();
+			return;
+		}
+	}
 }
