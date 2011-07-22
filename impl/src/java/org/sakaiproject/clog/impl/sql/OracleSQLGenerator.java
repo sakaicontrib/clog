61813e28629ca0757075c9977985aa6756b4942b
a.fish@lancaster.ac.uk <a.fish@lancaster.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2011-07-22 10:05:59 +0000

    Mapped TINYINT to NUMBER.

--- impl/src/java/org/sakaiproject/clog/impl/sql/OracleSQLGenerator.java
+++ impl/src/java/org/sakaiproject/clog/impl/sql/OracleSQLGenerator.java
@@ -2,8 +2,9 @@ package org.sakaiproject.clog.impl.sql;
 
 public class OracleSQLGenerator extends SQLGenerator {
     public OracleSQLGenerator() {
-	BLOB = "BLOB";
+    BLOB = "BLOB";
 	BIGINT = "NUMBER";
+	TINYINT = "NUMBER";
 	TIMESTAMP = "TIMESTAMP";
 	VARCHAR = "VARCHAR2";
 	TEXT = "LONG";
