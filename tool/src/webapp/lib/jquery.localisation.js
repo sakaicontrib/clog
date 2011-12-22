2d657e7667b158aac0e5e7896c5e8f24e5cf2e07
a.fish@lancaster.ac.uk <a.fish@lancaster.ac.uk@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2011-12-22 09:33:09 +0000

    CLOG-77
    Updated versions of jquery and included json2.js. Patch supplied by Wolfgang Rohregger.

--- tool/src/webapp/lib/jquery.localisation.js
+++ tool/src/webapp/lib/jquery.localisation.js
@@ -1,77 +1,93 @@
 /* http://keith-wood.name/localisation.html
-   Localisation assistance for jQuery v1.0.2.
-   Written by Keith Wood (kbwood@iprimus.com.au) June 2007. 
+   Localisation assistance for jQuery v1.0.5.
+   Written by Keith Wood (kbwood{at}iinet.com.au) June 2007. 
    Dual licensed under the GPL (http://dev.jquery.com/browser/trunk/jquery/GPL-LICENSE.txt) and 
    MIT (http://dev.jquery.com/browser/trunk/jquery/MIT-LICENSE.txt) licenses. 
    Please attribute the author if you use it. */
 
-(function($)
-{ // Hide scope, no $ conflict
+(function($) { // Hide scope, no $ conflict
 
 /* Load applicable localisation package(s) for one or more jQuery packages.
    Assumes that the localisations are named <base>-<lang>.js
    and loads them in order from least to most specific.
-   For example, $.localise('jquery-calendar');
+   For example, $.localise('mypackage');
    with the browser set to 'en-US' would attempt to load
-   jquery-calendar-en.js and jquery-calendar-en-US.js.
+   mypackage-en.js and mypackage-en-US.js.
    Also accepts an array of package names to process.
    Optionally specify whether or not to include the base file,
    the desired language, and/or the timeout period, e.g.
-   $.localise(['jquery-calendar', 'jquery-timeentry'], 
-      {loadBase: true; language: 'en-AU', timeout: 300}); */
-	$.localise = function(pkg, settings)
-	{
-		var saveSettings = {async: $.ajaxSettings.async, timeout: $.ajaxSettings.timeout};
-		$.ajaxSetup({async: false, timeout: (settings && settings.timeout ? settings.timeout : 500)});
-		var localiseOne = function(pkg, lang)
-		{
-			if (settings && settings.loadBase)
-			{
-				$.getScript(pkg + '.js');
-			}
-			if (lang.length >= 2)
-			{
-				$.getScript(pkg + '-' + lang.substring(0, 2) + '.js');
-			}
-			if (lang.length >= 5)
-			{
-				$.getScript(pkg + '-' + lang.substring(0, 5) + '.js');
-			}
-		};
-		var lang = normaliseLang(settings && settings.language ? settings.language : $.defaultLanguage);
-		if (isArray(pkg))
-		{
-			for (i = 0; i < pkg.length; i++)
-			{
-				localiseOne(pkg[i], lang);
-			}
+   $.localise(['mypackage1', 'yourpackage'], 
+      {loadBase: true; language: 'en-AU', timeout: 300});
+   @param  packages  (string or string[]) names of package(s) to load
+   @param  settings  omit for the current browser language or
+                     (string) code for the language to load (aa[-AA]) or
+                     (object} options for the call with
+					   language  (string) the code for the language
+					   loadBase  (boolean) true to also load the base package or false (default) to not
+                       path      (string or string[2]) the paths to the JavaScript,
+                                 either as both or [base, localisations]
+					   timeout   (number) the time period in milliseconds (default 500)
+   @param  loadBase  (boolean, optional) true to also load the base package or false (default) to not -
+                     omit this if settings is an object
+   @param  path      (string or string[2], optional) the paths to the JavaScript,
+                     either as both or [base, localisations] -
+                     omit this if settings is an object
+   @param  timeout   (number, optional) the time period in milliseconds (default 500) -
+                     omit this if settings is an object */
+$.localise = function(packages, settings, loadBase, path, timeout) {
+	if (typeof settings != 'object' && typeof settings != 'string') {
+		timeout = path;
+		path = loadBase;
+		loadBase = settings;
+		settings = '';
+	}
+	if (typeof loadBase != 'boolean') {
+		timeout = path;
+		path = loadBase;
+		loadBase = false;
+	}
+	if (typeof path != 'string' && !$.isArray(path)) {
+		timeout = path;
+		path = ['', ''];
+	}
+	settings = (typeof settings != 'string' ? settings || {} :
+		{language: settings, loadBase: loadBase, path: path, timeout: timeout});
+	var paths = (!settings.path ? ['', ''] :
+		($.isArray(settings.path) ? settings.path : [settings.path, settings.path]));
+	var opts = {async: false, dataType: 'script',
+		timeout: (settings.timeout != null ? settings.timeout : 500)};
+	var localiseOne = function(package, lang) {
+		if (settings.loadBase) {
+			$.ajax($.extend(opts, {url: paths[0] + package + '.js'}));
 		}
-		else
-		{
-			localiseOne(pkg, lang);
+		if (lang.length >= 2) {
+			$.ajax($.extend(opts, {url: paths[1] + package + '-' + lang.substring(0, 2) + '.js'}));
 		}
-		$.ajaxSetup(saveSettings);
+		if (lang.length >= 5) {
+			$.ajax($.extend(opts, {url: paths[1] + package + '-' + lang.substring(0, 5) + '.js'}));
+		}
 	};
-
-	/* Retrieve the default language set for the browser. */
-	$.defaultLanguage = normaliseLang(navigator.language ? navigator.language /* Mozilla */ :
-		navigator.userLanguage /* IE */);
-
-	/* Ensure language code is in the format aa-AA. */
-	function normaliseLang(lang)
-	{
-		lang = lang.replace(/_/, '-').toLowerCase();
-		if (lang.length > 3)
-		{
-			lang = lang.substring(0, 3) + lang.substring(3).toUpperCase();
-		}
-		return lang;
+	var lang = normaliseLang(settings.language || $.localise.defaultLanguage);
+	packages = ($.isArray(packages) ? packages : [packages]);
+	for (i = 0; i < packages.length; i++) {
+		localiseOne(packages[i], lang);
 	}
+};
 
-	/* Determine whether an object is an array. */
-	function isArray(a)
-	{
-		return (a.constructor && a.constructor.toString().match(/\Array\(\)/));
+// Localise it!
+$.localize = $.localise;
+
+/* Retrieve the default language set for the browser. */
+$.localise.defaultLanguage = normaliseLang(navigator.language /* Mozilla */ ||
+	navigator.userLanguage /* IE */);
+
+/* Ensure language code is in the format aa-AA. */
+function normaliseLang(lang) {
+	lang = lang.replace(/_/, '-').toLowerCase();
+	if (lang.length > 3) {
+		lang = lang.substring(0, 3) + lang.substring(3).toUpperCase();
 	}
+	return lang;
+}
 
 })(jQuery);
