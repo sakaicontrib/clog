cc8c931839ae0e0f7c8748b73fd2b71c777cd0a0
austin48 <austin48@hawaii.edu>
2021-03-04 02:07:35 -1000

    CLOG-205 post visibility option is not retained when re-editing. ensure new post select 'all' by default (#55)

--- tool/src/webapp/js/clog_utils.js
+++ tool/src/webapp/js/clog_utils.js
@@ -575,3 +575,11 @@ Handlebars.registerHelper('translate', function (key) {
 Handlebars.registerHelper('escapequote', function(variable) {
   return variable.replace(/\'/g, "\\'");
 });
+
+Handlebars.registerHelper('if_eq', function(a, b, options) {
+    if (a == b) {
+        return options.fn(this);
+    } else {
+        return options.inverse(this);
+    }
+});
